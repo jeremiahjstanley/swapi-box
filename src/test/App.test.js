f6c24ec3bcd08e5311f7349fca2419fa3a24573d
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-02 17:43:06 -0600

    Install dependencies and create basic file structure

---
+++ src/test/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../Components/App/App';
+
+it('renders without crashing', () => {
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+});
