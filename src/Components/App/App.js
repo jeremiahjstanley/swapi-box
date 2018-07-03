72ceafecfebc32610812bb864b4e53e38d94983d
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-02 18:07:01 -0600

    Outline basic app structure in app render method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,18 +11,23 @@ class App extends Component {
                 climate: 'temperate',
                 residents: [ 'Leia Organa' ],
                 favorite: false }]
-              }
+      scroll: {}
+    }
+  }
+
+  componentDidMount() {
+    this.setState({scroll: {title: 'A New Hope',
+                            releaseDate: '1977-05-25',
+                            scrollText: 'It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal secret\r\nplans to the Empire\'s\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough power\r\nto destroy an entire planet.\r\n\r\nPursued by the Empire\'s\r\nsinister agents, Princess\r\nLeia races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save her\r\npeople and restore\r\nfreedom to the galaxy....' }
+                 })
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Scroll scroll={this.state.scroll}/>
+        <Header/>
+        <CardContainer cards={this.state.cards}/>
       </div>
     );
   }
