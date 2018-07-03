c792589b24adb4ba09e89f63a1485e9bc5db90d6
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-03 12:10:13 -0600

    Add people data cleaner method

--- src/Components/App/helper.js
+++ src/Components/App/helper.js
@@ -3,7 +3,7 @@
 
 export const scroll = (data) => {
   const scrollInfo = data.results.reduce((scrollInfo, film) => {
-      if (film.title == "A New Hope") {
+      if (film.title === "A New Hope") {
         scrollInfo = {
                       title: film.title,
                       releaseDate: film.release_date,
@@ -15,3 +15,23 @@ export const scroll = (data) => {
   return scrollInfo;
 }
 
+export const people = (data) => {
+  const characters = data.results.map(character => {
+    return {
+            name: character.name,
+            homeworld: character.homeworld,
+            species: character.species,
+            population: character.homeworld,
+            favorite: false
+          }
+  })
+  return characters;
+}
+
+export const planets = (data) => {
+  console.log(data)
+}
+
+export const vehicles = (data) => {
+  console.log(data)
+}
