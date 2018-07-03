fe6258f31af0f44c23225e338d5f6e9b1917eda0
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-03 16:42:47 -0600

    Refactor helper.js fetch calls to use async/await snytax

--- src/Components/App/helper.js
+++ src/Components/App/helper.js
@@ -1,6 +1,4 @@
 
-
-
 export const scroll = (data) => {
   const scrollInfo = data.results.reduce((scrollInfo, film) => {
       if (film.title === "A New Hope") {
@@ -16,30 +14,58 @@ export const scroll = (data) => {
 }
 
 export const people = (data) => {
-  const people = data.results.map(person => {
+  const people = data.results.map(async person => {
+    const homeworld = await fetchData(person.homeworld)
+    const species = await fetchData(person.species)
+    console.log(homeworld, species)
     return {
             name: person.name,
-            homeworld: person.homeworld,
-            species: person.species,
-            population: person.homeworld,
+            homeworld: homeworld.name,
+            species: species.name,
+            population: homeworld.population,
             favorite: false
           }
   })
-  return people;
+
+  return Promise.all(people);
+}
+
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export const planets = (data) => {
-  const planets = data.results.map(planet => {
+  const planets = data.results.map(async planet => {
+    const residents = await fetchResidents(planet.residents)
     return {
       name: planet.name,
       terrain: planet.terrain,
       population: planet.population,
       climate: planet.climate,
-      residents: planet.residents,
+      residents: residents,
       favorite: false
     }
   })
   return planets
+}
+
+const fetchResidents = (residents) => {
+  const residentNames = residents.map(async resident => {
+    try {
+      const response = await fetch(resident);
+      const results = await response.json();
+      return results.name 
+    } catch (error) {
+      console.error(error)
+    }
+  })
+  return Promise.all(residentNames)
 }
 
 export const vehicles = (data) => {
@@ -48,9 +74,11 @@ export const vehicles = (data) => {
             name: vehicle.name,
             model: vehicle.model,
             vehicleClass: vehicle.vehicle_class,
-            numberOfPassengers: parseInt(vehicle.crew) + parseInt(vehicle.passengers),
+            numberOfPassengers: parseInt(vehicle.crew, 10) + parseInt(vehicle.passengers, 10),
             favorite: false
           }  
   })
   return vehicles
 }
+
+
