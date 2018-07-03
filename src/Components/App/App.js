266532fb10796ddbe6ec2bd71f36587dded4c6e3
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-02 18:48:12 -0600

    Add API fetch call in componentDidMount method in App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Scroll from '../Scroll/Scroll'
 import Header from '../Header/Header'
 import CardContainer from '../CardContainer/CardContainer'
+import { scroll } from './helper.js'
 import './App.css';
 
 class App extends Component {
@@ -19,10 +20,12 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({scroll: {title: 'A New Hope',
-                            releaseDate: '1977-05-25',
-                            scrollText: 'It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal secret\r\nplans to the Empire\'s\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough power\r\nto destroy an entire planet.\r\n\r\nPursued by the Empire\'s\r\nsinister agents, Princess\r\nLeia races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save her\r\npeople and restore\r\nfreedom to the galaxy....' }
-                 })
+    fetch('https://swapi.co/api/films/')
+      .then(data => data.json())
+      .then(parsedData => {
+        this.setState({ scroll: scroll(parsedData)})
+      })
+      .catch(error => console.error('Error:', error))
   }
 
   render() {
--- src/Components/App/helper.js
+++ src/Components/App/helper.js
@@ -1,11 +1,17 @@
 
-const scroll = data.results.reduce((scrollInfo, film) => {
-    if (film.title == "A New Hope") {
-      scrollInfo = {
-                    title: film.title,
-                    releaseDate: film.release_date,
-                    scrollText: film.opening_crawl,
-                  }
-    }
-    return scrollInfo
-}, {})
+
+
+export const scroll = (data) => {
+  const scrollInfo = data.results.reduce((scrollInfo, film) => {
+      if (film.title == "A New Hope") {
+        scrollInfo = {
+                      title: film.title,
+                      releaseDate: film.release_date,
+                      scrollText: film.opening_crawl,
+                    }
+      }
+      return scrollInfo;
+  }, {});
+  return scrollInfo;
+}
+
