cf93120f2b396af37a1d0b2db11954071e35d114
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-09 01:21:33 -0600

    Lint helper file

--- src/Components/App/helper.js
+++ src/Components/App/helper.js
@@ -1,47 +1,48 @@
-import { makePersonCard, makeVehicleCard, makePlanetCard } from './data-cleaner'
-import { fetchHomeworld, fetchResidents, fetchSpecies} from './api-calls'
+import {makePersonCard, makeVehicleCard, makePlanetCard} from './data-cleaner';
+import {fetchHomeworld, fetchResidents, fetchSpecies} from './api-calls';
 
 export const determineDataSet = (type, parsedData) => {
-    switch (type) {
-  case 'people':
-     return people(parsedData)
-  case 'planets':
-      return planets(parsedData)
-  case 'vehicles':
-      return vehicles(parsedData)
-  default: 
-      return console.log('error')
-    }
-}
+  switch (type) {
+    case 'people':
+      return people(parsedData);
+    case 'planets':
+      return planets(parsedData);
+    case 'vehicles':
+      return vehicles(parsedData);
+    default: 
+      throw new Error('Error');
+  }
+};
 
-export const people = async (data) => {
-  const people = data.results
-  const cleanPeople = people.map(person => cleanPerson(person))
-  return Promise.all(cleanPeople)
-}
+export const people = async (parsedData) => {
+  const people = parsedData.results;
+  const cleanPeople = people.map(person => cleanPerson(person));
+  return Promise.all(cleanPeople);
+};
 
-export const planets = async (data) => {
-  const planets = data.results
-  const cleanPlanets = planets.map(planet => cleanPlanet(planet))
+export const planets = async (parsedData) => {
+  const planets = parsedData.results;
+  const cleanPlanets = planets.map(planet => cleanPlanet(planet));
   return Promise.all(cleanPlanets);
-}
+};
 
-export const vehicles = (data) => {
-  const vehicles = data.results
-  const cleanVehicles = vehicles.map(vehicle => makeVehicleCard(vehicle))
-  return Promise.all(cleanVehicles)
-}
+export const vehicles = (parsedData) => {
+  const vehicles = parsedData.results;
+  const cleanVehicles = vehicles.map(vehicle => makeVehicleCard(vehicle));
+  return Promise.all(cleanVehicles);
+};
 
 export const cleanPerson = async (person) => {
-  const cleanHomeworld = await fetchHomeworld(person.homeworld)
-  const cleanSpecies = await fetchSpecies(person.species)
-  return makePersonCard({...person, homeworld: cleanHomeworld, species: cleanSpecies})
-} 
+  const cleanHomeworld = await fetchHomeworld(person.homeworld);
+  const cleanSpecies = await fetchSpecies(person.species);
+  return makePersonCard({...person, 
+    homeworld: cleanHomeworld, species: cleanSpecies});
+}; 
 
 export const cleanPlanet = async (planet) => {
-  const cleanResidents = await fetchResidents(planet.residents)
-  return makePlanetCard({...planet, residents: cleanResidents})
-}
+  const cleanResidents = await fetchResidents(planet.residents);
+  return makePlanetCard({...planet, residents: cleanResidents});
+};
 
 
 
