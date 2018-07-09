ef9128859d16949ccb375e6a42c93f03eb13de3a
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-09 01:33:29 -0600

    Lint Header Component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,51 +1,51 @@
 import React from 'react';
-import PropTypes from 'prop-types'
-import '../../reset.css'
-import './Header.css'
+import PropTypes from 'prop-types';
+import '../../reset.css';
+import './Header.css';
 
 const Header = ({buttons, displayCards, favorites}) => {
 
   const handleClick = (button) => {
-    buttons.map(button => button.active = false)
+    buttons.map(button => button.active = false);
     button.active = true;
     displayCards(button.name);
-  }
+  };
 
   const determineButton = (button) => {
     if (button.name === 'favorites') {
       return (        
         <span>
-         {favorites.length}
+          { favorites.length }
         </span>
-      )
-    }
-  }
+      );
+    };
+  };
 
   const clickButtons = buttons.map((button, index) => {
     return (
       <button
         className={button.active ? `button button${index} active` : `button button${index}`}
         key={index}
-        onClick={() => {handleClick(button)}}
+        onClick={() => { handleClick(button) }}
       >
         {button.name}
-        {determineButton(button)}
+        { determineButton(button) }
       </button>
-    )
-  })
+    );
+  });
 
-return (
-  <div className='header'>
-    {clickButtons}
-  </div>
-  )
-}
+  return (
+    <div className='header'>
+      {clickButtons}
+    </div>
+  );
+};
 
 Header.propTypes = {
   buttons: PropTypes.array.isRequired,
   displayCards: PropTypes.func.isRequired,
   favorites: PropTypes.array.isRequired
-}
+};
 
 
 export default Header;
