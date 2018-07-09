53b7c7f5e33c533cafcf7cc3a1323a53c6ea39e4
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-09 00:55:50 -0600

    Refactor mock data files to include results key with single array length value

--- src/data/mockPeople.js
+++ src/data/mockPeople.js
@@ -31,7 +31,8 @@ export const personInstance = {
             "url": "https://swapi.co/api/people/1/"
         }
 
-export const personInstanceArray = [{
+export const personInstanceArray = {
+        results: [{
             "name": "Luke Skywalker",
             "height": "172",
             "mass": "77",
@@ -40,7 +41,7 @@ export const personInstanceArray = [{
             "eye_color": "blue",
             "birth_year": "19BBY",
             "gender": "male",
-            "homeworld": "https://swapi.co/api/planets/1/",
+            "homeworld": {"name": "Tatooine", "population": "200000"},
             "films": [
                 "https://swapi.co/api/films/2/",
                 "https://swapi.co/api/films/6/",
@@ -48,9 +49,7 @@ export const personInstanceArray = [{
                 "https://swapi.co/api/films/1/",
                 "https://swapi.co/api/films/7/"
             ],
-            "species": [
-                "https://swapi.co/api/species/1/"
-            ],
+            "species": {"name": "human"},
             "vehicles": [
                 "https://swapi.co/api/vehicles/14/",
                 "https://swapi.co/api/vehicles/30/"
@@ -62,7 +61,9 @@ export const personInstanceArray = [{
             "created": "2014-12-09T13:50:51.644000Z",
             "edited": "2014-12-20T21:17:56.891000Z",
             "url": "https://swapi.co/api/people/1/"
-        }]
+            }]
+        }
+
 
 export const peopleFull = {
     "count": 87,
