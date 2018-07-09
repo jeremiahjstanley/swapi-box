79b83452e80309c31e9c79bd0657d570426948de
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-09 01:25:52 -0600

    Lint Card Component

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -7,9 +7,9 @@ const Card = ({card, addToFavorites, removeFromFavorites}) => {
   const handleClick = (card) => {
     card.favorite = !card.favorite;
     card.favorite ? addToFavorites(card) : removeFromFavorites(card.name);
-  }
+  };
 
-  switch(card.type) {
+  switch (card.type) {
     case 'vehicle': 
       return (
         <div className="card vehicleCard">
@@ -18,54 +18,54 @@ const Card = ({card, addToFavorites, removeFromFavorites}) => {
           <p>{card.vehicleClass}</p>
           <p>{card.numberOfPassengers}</p>
           <button 
-            onClick={() => {handleClick(card)}}
+            onClick={() => { handleClick(card) }}
             className={card.favorite.toString()}
           >
             &#9734;  
           </button>
         </div>
-      )
-  case 'planet':
-    return (
-      <div className="card planetCard">
-        <h3>{card.name}</h3>
-        <p>{card.terrain}</p>
-        <p>{card.population}</p>
-        <p>{card.climate}</p>
-        <p>{card.residents.map(resident => resident.name + ', ')}</p>
-        <button 
-          onClick={() => {handleClick(card)}}
-          className={card.favorite.toString()}
-        >
-          &#9734;  
-        </button>
-      </div>
-    )  
-  case 'person':
-    return (
-      <div className="card characterCard">
-        <h3>{card.name}</h3>
-        <p>{card.homeworld}</p>
-        <p>{card.species}</p>
-        <p>{card.population}</p>
-        <button 
-          onClick={() => {handleClick(card)}}
-          className={card.favorite.toString()}
-        >
-          &#9734;
-        </button>
-      </div>
-    )
-  default: 
-    return console.error()  
+      );
+    case 'planet':
+      return (
+        <div className="card planetCard">
+          <h3>{card.name}</h3>
+          <p>{card.terrain}</p>
+          <p>{card.population}</p>
+          <p>{card.climate}</p>
+          <p>{card.residents.map(resident => resident.name + ', ')}</p>
+          <button 
+            onClick={() => { handleClick(card) }}
+            className={card.favorite.toString()}
+          >
+            &#9734;  
+          </button>
+        </div>
+      );  
+    case 'person':
+      return (
+        <div className="card characterCard">
+          <h3>{card.name}</h3>
+          <p>{card.homeworld}</p>
+          <p>{card.species}</p>
+          <p>{card.population}</p>
+          <button 
+            onClick={() => { handleClick(card) }}
+            className={card.favorite.toString()}
+          >
+            &#9734;
+          </button>
+        </div>
+      );
+    default: 
+      throw new Error('Error'); 
   }
-}
+};
 
 Card.propTypes = {
   card: PropTypes.object.isRequired,
   addToFavorites: PropTypes.func.isRequired,
   removeFromFavorites: PropTypes.func.isRequired
-}
+};
 
 export default Card;
 
