920de0be10bad34b399beb13851fed0defff7fd3
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-09 00:50:47 -0600

    Add basic styling to App Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -58,7 +58,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="app-header">SWAPI-Box</h1>
+        <h1 className="app-header">STAR WARS</h1>
         <Scroll scroll={this.state.scroll}/>
         <Header 
           buttons={this.state.buttons}
