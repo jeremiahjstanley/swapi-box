38b6909140eb1ceb94a827003a553f0c770c86bf
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-09 01:10:41 -0600

    Lint api-calls file

--- src/Components/App/api-calls.js
+++ src/Components/App/api-calls.js
@@ -1,36 +1,36 @@
-import { makeScroll } from './data-cleaner'
-import { determineDataSet } from './helper.js'
+import { makeScroll } from './data-cleaner';
+import { determineDataSet } from './helper.js';
 
 export const getOpeningScroll = async () => {
-  const url = 'https://swapi.co/api/films/'
+  const url = 'https://swapi.co/api/films/';
   const response = await fetch(url);
-  const data = await response.json();
-  return makeScroll(data)
-}
+  const parsedData = await response.json();
+  return makeScroll(parsedData);
+};
 
 export const fetchData = async (type) => {
-  const url = `https://swapi.co/api/${type}/`
+  const url = `https://swapi.co/api/${type}/`;
   const response = await fetch(url);
-  const data = await response.json();
-  return determineDataSet(type, data)
-}      
+  const parsedData = await response.json();
+  return determineDataSet(type, parsedData);
+};     
 
 export const fetchHomeworld = async (homeworld) => {
-  const response = await fetch(homeworld)
-  const data = await response.json();
-  return data;
-}
+  const response = await fetch(homeworld);
+  const parsedData = await response.json();
+  return parsedData;
+};
 
 export const fetchSpecies = async (species) => {
-  const response = await fetch(species)
-  const data = await response.json();
-  return data;
-}
+  const response = await fetch(species);
+  const parsedData = await response.json();
+  return parsedData;
+};
 
 export const fetchResidents = (residents) => {
   const residentNames = residents.map(async resident => {
     const response = await fetch(resident);
     return await response.json();
-  })
-  return Promise.all(residentNames)
-}
+  });
+  return Promise.all(residentNames);
+};
